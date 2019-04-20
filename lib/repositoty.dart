971e320d5f66de4878731437e41a3acda45ba019
pedromassango <pedromassango.developer@gmail.com>
2019-04-20 23:08:34 +0100

    loading data from google books using http package

---
+++ lib/repositoty.dart
@@ -0,0 +1,36 @@
+
+import 'dart:async';
+
+import 'package:booksy/home/bloc/HomePageState.dart';
+import 'package:booksy/models.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert' as converter;
+
+class Repository{
+
+  final String baseUrl = "https://www.googleapis.com/books/v1/volumes";
+
+  Future<HomePageState> getBooks(String query) async{
+
+    final url = "$baseUrl?q=$query";
+    var response = await http.get(url);
+
+    if(response.statusCode == 200){
+      var body = response.body;
+      var json = converter.jsonDecode(body);
+      var data = BooksApiModel.fromJson(json);
+
+      var books = data.items;
+
+      return HomePageStateSuccess(
+        books: books
+      );
+    }else{
+      return HomePageStateError(
+        message: "Internet Error!"
+      );
+    }
+  }
+}
